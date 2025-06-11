dfa1ce4f2a21c6a0303ec7035c10a8af994d4eba
Yuseunggeun <yuu3730@naver.com>
2025-06-11 21:39:20 +0900

    feat: 서버 초기 설정

---
+++ src/main/java/com/ysg/rag_release_helper/RagReleaseHelperApplication.java
@@ -0,0 +1,13 @@
+package com.ysg.rag_release_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagReleaseHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagReleaseHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysg/rag_release_helper/RagReleaseHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysg.rag_release_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagReleaseHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
